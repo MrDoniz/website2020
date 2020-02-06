fdb748c1ac81728e3da6b09171afb6134b5ef41a
MrDoniz <danieldonizgarcia@gmail.com>
2020-02-06 23:28:12 +0000

    Visual Index

--- index.php
+++ index.php
@@ -1,31 +1,36 @@
 <!doctype html>
 <html lang="es">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="css/slider.css" />
-    <link rel="stylesheet" type="text/css" href="css/custom.css" />
-    <title>Darkness Of Glory</title>
-  </head>
-  <body style="background-color:black; overflow: hidden;">
-    <!-- Nav -->
-    <button type="button" class="btn btn-link float-sm-right" style="color:#ffc107">If you are registered  >></button>
-    <div class="container">
-      <div class="row h-50" style="padding:5%;display:inherit;">
-        <div class="d-table-cell align-middle" style="background-color:rgba(0,0,0,0.7);">
-          <img src="images/logo/logo.png" class="img-fluid mx-auto d-block" alt="..." style="max-width:50%;height:auto">
-          <img src="images/logo/logo2.png" class="img-fluid mx-auto d-block" alt="..." style="max-width:50%;height:auto">
-          <button type="button" class="btn btn-outline-warning mx-auto d-block" style="margin:20px">Play for Free</button>
-        </div>
-      </div>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+  <link rel="stylesheet" type="text/css" href="css/slider.css" />
+  <link rel="stylesheet" type="text/css" href="css/custom.css" />
+  <title>Darkness Of Glory</title>
+</head>
+<body>
+  <div class="row justify-content-center">
+    <div class="col-md-12">
+			<nav class="navbar navbar-expand-lg">
+        <ul class="navbar-nav ml-md-auto">
+						<li class="nav-item active">
+							 <button type="button" class="btn btn-link float-sm-right" style="color:#ffc107">If you are registered  >></button>
+						</li>
+					</ul>
+			</nav>
     </div>
-    <!-- Background -->
-    <?php require('script/slider.php')?>
-  </body>
-  <footer>
-    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-  </footer>
+    <div class="col-md-6 background0">
+      <img src="images/logo/logo.png" class="img-fluid mx-auto d-block" alt="..." style="max-width:50%;height:auto">
+      <img src="images/logo/logo2.png" class="img-fluid mx-auto d-block" alt="..." style="max-width:50%;height:auto">
+      <button type="button" class="btn btn-outline-warning mx-auto d-block" style="margin:20px">Play for Free</button>
+    </div>
+  </div>
+  <!-- Background -->
+  <?php require('script/slider.php')?>
+</body>
+<footer>
+  <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+</footer>
 </html>
